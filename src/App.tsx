9d4d383dcd21710f1f9f481631dea7e91f30fab9
Poison02 <2069820192@qq.com>
2024-01-06 16:36:01 +0800

    重新初始化项目

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React, { Suspense, memo } from 'react'
+import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-import { useRoutes } from 'react-router-dom'
-
-import routes from './routes'
-import LoadingPage from './views/loding'
 import './App.scss'
 
 interface IProps {
@@ -11,7 +7,7 @@ interface IProps {
 }
 
 const App: FC<IProps> = () => {
-  return <Suspense fallback={<LoadingPage />}>{useRoutes(routes)}</Suspense>
+  return <div>App</div>
 }
 
 export default memo(App)
--- src/api/index.ts
+++
@@ -1,2 +0,0 @@
-export * as login from './login'
-export * as system from './system'
