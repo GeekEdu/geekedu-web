8f567dc6992e1c16bd784cb49591fd9b0ce36b62
Poison02 <2069820192@qq.com>
2024-03-18 15:43:27 +0800

    优化代码

--- src/pages/order/success.tsx
+++ src/pages/order/success.tsx
@@ -2,7 +2,7 @@
  * @Author: Poison02 2069820192@qq.com
  * @Date: 2024-01-19 22:55:05
  * @LastEditors: Poison02 2069820192@qq.com
- * @LastEditTime: 2024-03-18 15:09:51
+ * @LastEditTime: 2024-03-18 15:43:10
  * @FilePath: /geekedu-web/src/pages/order/success.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -27,10 +27,10 @@ function OrderSuccessPage() {
         <div className={styles['success-info']}>
           <img src={successIcon} />
           {tradeStatus}
-          !!!
+          ！！！
         </div>
         <div className={styles.tradeNo}>
-          订单号：
+          交易订单号：
           {tradeNo}
         </div>
         <div className={styles.price}>
